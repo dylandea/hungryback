7953aef4592b974a96cb970365f07948c046d6f6
Dylan <pinpon@ordi-pinpon.local>
2022-11-19 04:33:00 +0100

    push github

--- routes/api.js
+++ routes/api.js
@@ -1,115 +1,142 @@
 var express = require("express");
 var router = express.Router();
 const { default: mongoose } = require("mongoose");
-const puppeteer = require("puppeteer");
+
 const treatText = require("./text_treatment_function");
 const treatWeb = require("./web_treatment_function");
-// const Tesseract = require("tesseract.js");
-const tesseract = require("node-tesseract-ocr");
+const Tesseract = require("tesseract.js");
+/* const tesseract = require("node-tesseract-ocr"); */
 
 //-------ROUTE TESSERACT - POST
 router.post("/tesseract", async function (req, res, next) {
-    var image = req.body.image;
+	var image = req.body.image;
 
-    const config = {
-        lang: "fra",
-        oem: 3,
-        psm: 3,
-    };
-    tesseract
-        .recognize(image, config)
-        .then((text) => {
-            var resultObj = treatText(text);
+	const config = {
+		lang: "fra",
+		oem: 3,
+		psm: 3,
+	};
+	/* 	tesseract
+		.recognize(image, config)
+		.then((text) => {
+			var resultObj = treatText(text);
 
-            res.json({ recipeTreated: resultObj });
-        })
-        .catch((error) => {
-            res.json({ message: "err" });
-        });
+			res.json({ recipeTreated: resultObj });
+		})
+		.catch((error) => {
+			res.json({ message: "err" });
+		}); */
 
-    // Tesseract.recognize(image, "fra", { logger: (m) => console.log(m) })
-    // 	.then(({ data: { text } }) => {
-    // 		var resultObj = treatText(text);
+	Tesseract.recognize(image, "fra", { logger: (m) => console.log(m) })
+		.then(({ data: { text } }) => {
+			var resultObj = treatText(text);
 
-    // 		res.json({ recipeTreated: resultObj });
-    // 	})
-    // 	.catch((error) => {
-    // 		res.json({ message: "err" });
-    // 	});
+			res.json({ recipeTreated: resultObj });
+		})
+		.catch((error) => {
+			res.json({ message: "err" });
+		});
 });
 
 //------ROUTE URL SCRAPPER-POST
 router.post("/url-scrapper", async function (req, res, next) {
-    const iPhone = puppeteer.devices["iPhone 13"];
+	
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    /*  await page.emulate(iPhone); */
+	let chrome = {};
+let puppeteer;
 
-    await page.goto(req.body.url);
-    const html = await page.content();
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  // running on the Vercel platform.
+  chrome = require('chrome-aws-lambda');
+  puppeteer = require('puppeteer-core');
+} else {
+  // running locally.
+  puppeteer = require('puppeteer');
+}
 
-    var htmlArr = html.split("\n");
-    htmlArr = htmlArr.filter((x) => x !== "");
-    var htmlArrShort = htmlArr.slice(0, 30);
-
-    var isImg = (element) =>
-        element.includes(
-            'meta property="og:image" content="https://www.ptitchef.com/imgupl/'
-        );
-    var indexImg = htmlArrShort.findIndex(isImg);
-    var img = htmlArrShort[indexImg];
-    let regExImg = /https:.+jpg/i;
-    let imgUrl = img.match(regExImg)[0];
-
-    //prend code source, mais certaines pages n'affichent rien comme React
-    // const extractedText = await page.$eval('*', (el) => el.innerText);
-
-    //fais un copier coller à la main
-    var extractedText1 = await page.$eval("*", (el) => {
-        const selection = window.getSelection();
-        const range = document.createRange();
-        range.selectNode(el);
-        selection.removeAllRanges();
-        selection.addRange(range);
-        return window.getSelection().toString();
+const getData = async (url) => {
+  try {
+    let browser = await puppeteer.launch({
+      args: [...chrome.args, '--hide-scrollbars', '--disable-web-security'],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
     });
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
 
-    // await page.screenshot({ path: './tmp/puppeteer/url_screenshot.jpg', fullPage: true });
-    // await page.pdf({ path: './tmp/puppeteer/puppet.pdf', format: 'a4' });
+	
+	const page = await browser.newPage();
+	/*  await page.emulate(iPhone); */
 
-    const dimensions = await page.evaluate(() => {
-        return {
-            width: document.documentElement.clientWidth,
-            height: document.documentElement.clientHeight,
-            deviceScaleFactor: window.devicePixelRatio,
-        };
-    });
+	await page.goto(req.body.url);
+	const html = await page.content();
 
-    await browser.close();
-    extractedText1 = extractedText1 + imgUrl;
-    var resultObj = treatWeb(extractedText1);
+	var htmlArr = html.split("\n");
+	htmlArr = htmlArr.filter((x) => x !== "");
+	var htmlArrShort = htmlArr.slice(0, 30);
 
-    res.json({ status: true, recipe: resultObj });
+	var isImg = (element) =>
+		element.includes(
+			'meta property="og:image" content="https://www.ptitchef.com/imgupl/'
+		);
+	var indexImg = htmlArrShort.findIndex(isImg);
+	var img = htmlArrShort[indexImg];
+	let regExImg = /https:.+jpg/i;
+	let imgUrl = img.match(regExImg)[0];
+
+	//prend code source, mais certaines pages n'affichent rien comme React
+	// const extractedText = await page.$eval('*', (el) => el.innerText);
+
+	//fais un copier coller à la main
+	var extractedText1 = await page.$eval("*", (el) => {
+		const selection = window.getSelection();
+		const range = document.createRange();
+		range.selectNode(el);
+		selection.removeAllRanges();
+		selection.addRange(range);
+		return window.getSelection().toString();
+	});
+
+	// await page.screenshot({ path: './tmp/puppeteer/url_screenshot.jpg', fullPage: true });
+	// await page.pdf({ path: './tmp/puppeteer/puppet.pdf', format: 'a4' });
+
+	const dimensions = await page.evaluate(() => {
+		return {
+			width: document.documentElement.clientWidth,
+			height: document.documentElement.clientHeight,
+			deviceScaleFactor: window.devicePixelRatio,
+		};
+	});
+
+	await browser.close();
+	extractedText1 = extractedText1 + imgUrl;
+	var resultObj = treatWeb(extractedText1);
+
+	res.json({ status: true, recipe: resultObj });
 });
 
 router.get("/tesseract", async function (req, res, next) {
-    var image = "./tests/images/ok/ochazuke_9.png";
+	var image = "./tests/images/ok/ochazuke_9.png";
 
-    const config = {
-        lang: "fra",
-        oem: 3,
-        psm: 3,
-    };
-    tesseract
-        .recognize(image, config)
-        .then((text) => {
-            var resultObj = treatText(text);
-            res.json({ resultObj });
-        })
-        .catch((error) => {
-            res.json({ message: "err" });
-        });
+	const config = {
+		lang: "fra",
+		oem: 3,
+		psm: 3,
+	};
+	tesseract
+		.recognize(image, config)
+		.then((text) => {
+			var resultObj = treatText(text);
+			res.json({ resultObj });
+		})
+		.catch((error) => {
+			res.json({ message: "err" });
+		});
 });
 
 module.exports = router;
